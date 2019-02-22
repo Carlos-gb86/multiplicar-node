2e6ad23d0f3bc022d389c5566583d78a328cf219
Berrocal <carlos.gil.berrocal@gmail.com>
2019-02-22 20:46:48 +0100

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const colors = require('colors');
+
+
+let listarTabla = (base, limite = 10) => {
+    if ( !Number(base) ) {
+        reject(`El valor introducido ${base} no es un número!`);
+        return
+    }
+
+    console.log('==============================================='.green);
+    console.log(`===== Table de Multiplicar del ${base} =============`.green);
+    console.log('==============================================='.green);
+
+    for (let i = 1; i<=limite; i++){
+        console.log(`${base} * ${i} = ${base*i}`);
+    }
+}
+
+let crearArchivo = ( base, limite = 10 ) => {
+    return new Promise( (resolve, reject) => {
+
+        if ( !Number(base) ) {
+            reject(`El valor introduce ${base} no es un número!`);
+            return
+        }
+
+        let data = '';
+
+        for (let i = 1; i<=limite; i++){
+            data += `${base} * ${i} = ${base*i}\n`;
+        }
+
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+
+            if (err) 
+                reject(err);
+            else 
+                resolve(`tabla-${base}.txt`);      
+            
+        });
+    });
+} 
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
